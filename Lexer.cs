ba92b53a4c653f0a5de0ca962269b40c8462ba0f
Jefferson Gomes <jeffsilvaygo@gmail.com>
2019-09-23 17:17:42 -0300

    incomplete class should look over ascii

--- Lexer.cs
+++ Lexer.cs
@@ -1,22 +1,23 @@
 using System;
 using System.IO;
+using System.Text;
 
 public class Lexer
 {
     private int lookHead, numLinha, numColuna;
     private StreamReader file;
+    private Simbol_Table ts = new Simbol_Table();
+    Encoding ascii = Encoding.ASCII;
 
     public Lexer(string arq)
     {      
         try
         {
-            file = File.OpenText(arq);
+            this.file = File.OpenText(arq);
 
-            lookHead = 0;
-            numLinha = 1;
-            numColuna = 1;
-
-            Simbol_Table ts = new Simbol_Table();
+            this.lookHead = 0;
+            this.numLinha = 1;
+            this.numColuna = 1;
         }
 
         catch (Exception ex)
@@ -27,14 +28,23 @@ public class Lexer
 
     public void closeFile()
     {
-        try
-        {
-            file.Close();
-        }
+        try { file.Close(); }
         
-        catch (Exception e)
-        {
-            Console.WriteLine("Erro ao fechar arquivo");
-        }
+        catch (Exception e) { Console.WriteLine("Erro ao fechar arquivo"); }
     }  
+
+    // imprime tabela de simbolos
+    public void printTS() { this.ts.printTokens(); }
+
+    /*
+        Metodo ainda não esta funcionando, deve olhar como mexe com a
+        tabela ascii no C#, pelo exemplo do professor, ele converte os
+        num inteiros para caracter.
+         
+    public void retornaPonteiro(){
+        byte[] bytes = {this.lookHead};
+        string teste = Encoding.ASCII.GetString(bytes);
+        Console.WriteLine(teste);
+    }
+    */
 }
