ddb81314b75737be613705c8e3cb3fb5041eddb9
Jefferson Gomes <jeffsilvaygo@gmail.com>
2019-09-23 17:19:04 -0300

    I renamed variables

--- Simbol_Table.cs
+++ Simbol_Table.cs
@@ -5,12 +5,12 @@ using System.Collections.Generic;
 public class Simbol_Table
 {
     private Hashtable table = new Hashtable(); 
-    private List<Token> parts; //Lista para armazenar o token e a posicao em que aparece
+    private List<Token> tokens; //Lista para armazenar o token e a posicao em que aparece
     private Token token;
 
     public Simbol_Table()
     { 
-        parts = new List<Token>();
+        this.tokens = new List<Token>();
 
         // Definindo o tokens
         definirToken("KW_IF", "if");
@@ -22,19 +22,19 @@ public class Simbol_Table
 
     public void definirToken(string chave, string lexema)
     {
-        token = new Token(chave, lexema, 0, 0);
-        parts.Add(token);
+        this.token = new Token(chave, lexema, 0, 0);
+        this.tokens.Add(token);
     }
 
     // Metodo que add token criado para a tabela de simbolos
-    public void addToken(string nomeTag, string lexema) { table.Add(nomeTag, lexema); }
+    public void addToken(string nomeTag, string lexema) { this.table.Add(nomeTag, lexema); }
 
 
     public void printTokens()
     {
-        foreach(Token dinosaur in parts)
+        foreach(Token tok in this.tokens)
         {
-            Console.WriteLine(dinosaur.toString();
+            Console.WriteLine(tok.toString());
         }
     }
 }
