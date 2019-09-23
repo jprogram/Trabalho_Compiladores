0eb772e3828edaf2647be19d9dd195c869b46adc
Jefferson Gomes <jeffsilvaygo@gmail.com>
2019-09-23 17:15:41 -0300

    main class

---
+++ Program.cs
@@ -0,0 +1,15 @@
+using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.Write((int)Tag.Tags.KW_IF);
+
+        Lexer s = new Lexer("arq1.txt");
+
+        s.printTS();
+
+        //s.retornaPonteiro();
+    }
+}
