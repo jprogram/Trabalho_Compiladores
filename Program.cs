564f2fa0c53801e7d115b93ee3e75dcba30d410a
Jefferson Gomes <jeffsilvaygo@gmail.com>
2019-09-24 11:28:19 -0300

    update main class

--- Program.cs
+++ Program.cs
@@ -4,6 +4,23 @@ public class Program
 {
     public static void Main(string[] args)
     {
+    	Lexer lex = new Lexer("HelloWorld.txt");
+
+    	Console.Write("\n=>Lista de tokens:");
+
+    	Token t = new Token();
+    	t = lex.proximoToken();
+
+    	while(t is not null && t.getNome() != Tag.Tags.EOF){
+    		Console.WriteLine(t.toString()+ "Linha: " + (str)t.getLinha() + "Coluna: "+ (str)t.getColuna());
+    	}
+
+    	Console.Write("\n=>Tabela de simbolos: ");
+    	lex.printTS();
+    	lex.closeFile();
+
+    	Console.WriteLine("\n=> Fim da Compilação");
+
         Console.Write((int)Tag.Tags.KW_IF);
 
         Lexer s = new Lexer("arq1.txt");
