37fb83265ebceec379e381c0868ccc407ba0f593
Jefferson Gomes <jeffsilvaygo@gmail.com>
2019-09-26 21:30:20 -0300

    update test main class

--- Program.cs
+++ Program.cs
@@ -7,26 +7,26 @@ public class Program
     	Lexer lex = new Lexer("HelloWorld.txt");
 		Tag tag = new Tag();
 
+		int ponteiro = 0;
+
     	Console.Write("\n=>Lista de tokens:");
 
-    	Token t = lex.proximoToken();
-
-		/* 
-		// Isso aqui são testes, pode ignorar
-		Console.Write(tag.getEOF() );
-		Console.WriteLine(t.toString() );		
-
+    	Token t = lex.proximoToken(ponteiro);
+		
 		// Modo de impressao dos tokens que ainda deve ser revisado
-    	while( t != null && !t.getNome().Equals( tag.getEOF() ) )
+    	while( t != null && !t.getNome().Equals( Tag.Tags.EOF.ToString() ) )
 		{
-    		Console.WriteLine(t.toString()+ "Linha: " + t.getLinha() + "Coluna: "+ t.getColuna() );
-			t = lex.proximoToken();
+			Console.WriteLine();
+    		Console.WriteLine(t.toString()+ " Linha: " + t.getLinha() + " Coluna: "+ t.getColuna() );
+			
+			ponteiro++;
+			t = lex.proximoToken(ponteiro);
     	}
-		*/
+
     	Console.Write("\n=>Tabela de simbolos: ");
     	lex.printTS();
     	lex.closeFile();
 
-    	Console.WriteLine("\n=> Fim da Compilação");
+    	Console.WriteLine("\n\n=> Fim da Compilação");
     }
 }
