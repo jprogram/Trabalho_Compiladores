7b83980db3e93b6f3f9709bd77d7c1df13fa2176
Jefferson Gomes <jeffsilvaygo@gmail.com>
2019-09-28 14:33:32 -0300

    change main class

--- Program.cs
+++ Program.cs
@@ -9,23 +9,14 @@ public class Program
 
     	Console.Write("\n=>Lista de tokens:");
 
-    	Token t = lex.proximoToken();
+    	Token token = lex.proximoToken();
 		
-		// Modo de impressao dos tokens que ainda deve ser revisado
-    	while( t != null && !t.getNome().Equals( Tag.Tags.EOF.ToString() ) )
+		// Modo de impressao dos tokens
+    	while( token != null )
 		{
 			Console.WriteLine();
-    		Console.WriteLine(t.toString()+ " Linha: " + t.getLinha() + " Coluna: "+ t.getColuna() );
-			t = lex.proximoToken();
-			/*  
-            // Isso aqui era para testar a logica para ler
-            // a proxima linha do arquivo txt e ficar
-            // montando os tokens e tals
-			if(t is null){
-				lex.lerProxLinha();
-				t = lex.proximoToken();
-			}
-			*/
+    		Console.WriteLine(token.toString()+ " Linha: " + token.getLinha() + " Coluna: "+ token.getColuna() );
+			token = lex.proximoToken();
     	}
 
     	Console.Write("\n=>Tabela de simbolos: ");
